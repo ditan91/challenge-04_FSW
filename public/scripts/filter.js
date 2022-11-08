736ee27c6573b5160c570664c24391f597d9244b
ditan91 <ditanoviyanti91@gmail.com>
2022-11-09 01:30:07 +0700

    update date

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -5,8 +5,10 @@ function listCar(){
     let date = document.getElementById('date').value
     let time = document.getElementById('time').value
     let driver = document.getElementById('driver').value
-    let currDate = new Date();
+    let currDate = new Date(); //object
+    let today = JSON.stringify(currDate).slice(1,11) //string
     
+    // read json data
     var xmlhttp = new XMLHttpRequest();
     var url = "http://localhost:8000/json";
 
@@ -17,16 +19,18 @@ function listCar(){
 
     var newHTML = ""
     card = document.getElementById("cars-container")
+
+    // chek if there is data that null
     if(capacity ==='' || date==='' || time==='' || driver === ''){
         alert("Fill all the blank")
-    } else if (date < currDate) {
-        alert("Choose another day")
+    } else if (Date.parse(date) < Date.parse(currDate)) {
+        alert("Day not available, choose another day")
     } else {
         for (let i = 0; i < json.length; i++) {
             var car = json[i];
             if (JSON.stringify(json[i].capacity) === capacity && json[i].available === true){
                 newHTML += `
-                <div class="card m-3" style="width: 18rem;height: 35rem;">
+                <div class="card m-3" style="width: 18rem;">
                     <img src="${car.image}" class="card-img-top m-1" alt="${car.manufacture}" style="object-fit: scale-down; height: 150px; ">
                     <div class="card-body">
                         <h5 class="card-title">${car.manufacture} ${car.model}</h5>
@@ -39,7 +43,7 @@ function listCar(){
                 </div>
                 `;
             }
-           
+
         }
         card.innerHTML = newHTML;
         if (card = ""){
